392d9c68aef8ae6f050bbef0dda97225a68aeb8e
Lin Jie <ljlovemichael@hotmail.com>
2017-01-06 01:16:55 +0800

    test

--- Threads/createthread/Program.cs
+++ Threads/createthread/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace createthread
@@ -10,6 +11,15 @@ namespace createthread
     {
         static void Main(string[] args)
         {
+            Thread thread = Thread.CurrentThread;
+            thread.Name = "Main Thread";
+            string threadMessage = string.Format("Thread ID:{0}\n    Current AppDomainId:{1}\n    " +
+                "Current ContextId:{2}\n    Thread Name:{3}\n    " +
+                "Thread State:{4}\n    Thread Priority:{5}\n",
+                thread.ManagedThreadId, Thread.GetDomainID(), Thread.CurrentContext.ContextID,
+                thread.Name, thread.ThreadState, thread.Priority);
+            Console.WriteLine(threadMessage);
+            Console.ReadKey();
         }
     }
 }
