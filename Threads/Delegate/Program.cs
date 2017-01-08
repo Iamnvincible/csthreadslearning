f1461de21f0c43917604a44751f07fc8fb2df900
Lin Jie <ljlovemichael@hotmail.com>
2017-01-08 23:43:20 +0800

    回忆和学习一点东西，乱七八糟

---
+++ Threads/Delegate/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    class Program
+    {
+        delegate int intresult(int x, int y);
+        static void Main(string[] args)
+        {
+            intresult a = hahaha;
+            int c = a(3, 4);
+            Action<int, int> baction = (x, y) =>
+              {
+                  Console.WriteLine(x + y);
+              };
+            baction(4, 5);
+            //baction.BeginInvoke(3, 4);
+
+
+            Console.WriteLine(c);
+            int xx = myfunc(x =>
+              {
+                  return x * x;
+              }, 3);
+            Console.WriteLine(xx);
+
+            Console.ReadKey();
+        }
+        static int hahaha(int x, int y)
+        {
+            return x + y;
+        }
+        static int myfunc(Func<int, int> f1, int x)
+        {
+            return f1(x);
+        }
+    }
+}
---
+++ Threads/Parallel/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Parallels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ParallelLoopResult result = Parallel.For(0, 10, i =>
+            {
+                Console.WriteLine("{0},task:{1},thread:{2}", i, Task.CurrentId, Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(10);
+            });
+            Console.WriteLine("is completed:{0}", result.IsCompleted);
+
+            var sw = new Stopwatch();
+            sw.Start();
+            Task.Delay(3000);
+            Console.WriteLine("async:running for {0} seconds", sw.Elapsed.TotalSeconds);
+            Thread.Sleep(1000);//这段时间阻塞线程，不能处理事情。。
+            Console.WriteLine("async:running for {0} seconds", sw.Elapsed.TotalSeconds);
+
+
+
+            ParallelLoopResult result2 = Parallel.For(10, 700, async (i, pls) =>
+                {
+                    Console.WriteLine("i:{0} task {1}", i, Task.CurrentId);
+                    await Task.Delay(10);
+                    if (i > 15) pls.Break();
+                });
+
+            Console.WriteLine("is completed:{0}", result2.IsCompleted);
+            Console.WriteLine("lowest break iteration:{0}", result2.LowestBreakIteration);
+            Console.ReadLine();
+
+        }
+    }
+}
